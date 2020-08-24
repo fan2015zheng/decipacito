98341987b908b62a225288d87de34dda9a5f583f
Fan Zheng <fan2015zheng@gmail.com>
2020-08-24 01:56:13 -0500

    only load when user action for Safari

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,14 +1,10 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef} from 'react'
 import './WordCard.css'
 
 function WordCard({word}) {
 
   const [showEng, setShowEng] = useState(false)
   const audioRef = useRef()
-
-  useEffect(() => {
-    audioRef.current.load()
-  })
   
   let image = ""
   let spanish = ""
@@ -53,7 +49,7 @@ function WordCard({word}) {
           onClick={()=> {setShowEng(!showEng)}}
         ></div>
         <div className="_audio _pointer" style={audioStyle}
-          onClick={() => {audioRef.current.play()}}
+          onClick={() => {audioRef.current.load(); audioRef.current.play()}}
         ></div>
         <audio ref={audioRef}>
           <source src={audio} type="audio/mpeg" />
