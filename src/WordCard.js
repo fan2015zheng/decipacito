c4fdecd65bc5d96c7bfd426f49c487bdfdbd14ba
Fan Zheng <fan2015zheng@gmail.com>
2020-08-24 02:19:26 -0500

    Revert "only load when user action for Safari"
    This reverts commit 98341987b908b62a225288d87de34dda9a5f583f.

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,10 +1,14 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import './WordCard.css'
 
 function WordCard({word}) {
 
   const [showEng, setShowEng] = useState(false)
   const audioRef = useRef()
+
+  useEffect(() => {
+    audioRef.current.load()
+  })
   
   let image = ""
   let spanish = ""
@@ -49,7 +53,7 @@ function WordCard({word}) {
           onClick={()=> {setShowEng(!showEng)}}
         ></div>
         <div className="_audio _pointer" style={audioStyle}
-          onClick={() => {audioRef.current.load(); audioRef.current.play()}}
+          onClick={() => {audioRef.current.play()}}
         ></div>
         <audio ref={audioRef}>
           <source src={audio} type="audio/mpeg" />
